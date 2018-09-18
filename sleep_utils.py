521c2a49fcc61da53419ffdb9ce18908f06e386e
Marco Brigham <marco@brigham.be>
2018-09-18 21:30:54 +0100

    Added cross-validation functions

--- sleep_utils.py
+++ sleep_utils.py
@@ -2,14 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import h5py
-from IPython.core.display import HTML
+import keras
+from keras.preprocessing.sequence import Sequence
+import random
 
-def load_dataset(path, exclude_record=None, channels_ref=None, remove_mean=False, verbose=True):
+
+metrics_short_ref = {'loss': 'Loss', 
+                     'categorical_accuracy': 'Accuracy'
+                    }
+
+
+def load_dataset(path, exclude_record=None, channels_ref=None, verbose=True):
     """Import HDF5 databases from path.
     path: path of folder containing HDF5 databases
     exclude_record: list of databases to exclude
     channels_ref: list of channels to include
-    remove_mean: remove mean channel value
     verbose: extensive reporting
 
     returns dataset: dictionary with contents of HDF5 databases in each key,
@@ -36,6 +43,9 @@ def load_dataset(path, exclude_record=None, channels_ref=None, remove_mean=False
 
         with h5py.File(record_path, 'r') as f:
 
+        if channels_ref is None:
+            channels_ref = list(f.keys())
+
             if verbose:
                 print(record_id)
                 
@@ -43,9 +53,6 @@ def load_dataset(path, exclude_record=None, channels_ref=None, remove_mean=False
               
                 data = f[channel][::]
                 data = data[:, np.newaxis, :]
-                
-                if remove_mean:
-                    data = data - np.mean(data)
 
                 if idx_channel==0:
                     x = data
@@ -66,7 +73,217 @@ def load_dataset(path, exclude_record=None, channels_ref=None, remove_mean=False
     return dataset
 
 
-def plot_stats(dataset, exclude_record=None, title=None,
+def to_input(u, tensor=False):
+    """Convert data array to shape (batch, data).
+    u: array with shape (batch, samples, channels)
+    
+    returns x_out: array x with shape (batch, samples*selected_channels) if tensor is False
+                   array x with shape (batch, samples, selected_channels, 1) if tensor is True"""
+    
+    if tensor:
+        u_out = u.reshape((u.shape[0], u.shape[1], u.shape[2], 1))
+        
+    else:
+        u_out = u.reshape((u.shape[0], u.shape[1]*u.shape[2]))
+    
+    return u_out
+
+
+def to_output(u, num_classes=5):
+    """Convert label array to one-hot-encoding with shape (batch, binary_label).
+    u: label array with shape (batch, 1)
+    
+    returns: u_out (array with shape (batch, binary_label))"""
+
+    u_out = keras.utils.to_categorical(u, num_classes=num_classes)
+    
+    return u_out
+
+  
+class PSGSequence(keras.preprocessing.sequence.Sequence):
+    
+    def __init__(self, dataset, records=None, epochs_bool=None, batch_size=32,
+                 tensor=False, process_input=to_input, process_output=to_output):
+    
+        self.dataset = dataset
+        self.records = records
+        self.epochs_bool = epochs_bool
+        self.batch_size = batch_size
+        self.tensor = tensor
+        self.process_input = process_input
+        self.process_output = process_output
+
+        if self.records is None:
+
+            self.records = list(self.dataset.keys())
+            self.epochs_bool = None
+
+        if self.epochs_bool is None:
+
+            self.epochs_bool = [np.array([True]*self.dataset[record]['hypnogram'].shape[0])
+                                for record in self.records]
+
+        self.records_epochs = []
+        for idx_record, record in enumerate(self.records):
+            epochs, = np.where(self.epochs_bool[idx_record])
+            self.records_epochs += [[idx_record, epoch] for epoch in epochs]
+                                
+        self.on_epoch_end()
+                                
+        self.epochs_n = len(self.records_epochs)
+        
+        x_batch, y_batch = self.__getitem__(0)
+        
+        self.x_batch_shape = x_batch.shape
+        self.y_batch_shape = y_batch.shape
+        
+    def on_epoch_end(self):
+        
+        random.shuffle(self.records_epochs)
+        
+    def __len__(self):
+        
+        return int(np.ceil(self.epochs_n / float(self.batch_size)))
+    
+    def __getitem__(self, idx):
+        
+        records_epochs_batch =  self.records_epochs[idx 
+                                                    * self.batch_size:(idx + 1) 
+                                                    * self.batch_size]
+                                
+        x_batch, y_batch = ([],[])
+        
+        for idx_record, epoch in records_epochs_batch:
+          
+            record = self.records[idx_record]
+            x_batch += [self.dataset[record]['channels'][epoch]]
+            y_batch += [self.dataset[record]['hypnogram'][epoch]]
+        
+        return (self.process_input(np.array(x_batch), tensor=self.tensor),
+                self.process_output(np.array(y_batch)))
+      
+  
+def cross_validation(dataset, model_ref,
+                     tensor=False, epochs=10,  batch_size=128, 
+                     plot=True, verbose=True):
+    """Leave-one-out cross validation scheme
+    dataset: dataset containing records with PSG data and hypnograms
+    model_ref: Keras model
+    tensor: provide tensor shape PSG data
+    epochs: number of training epochs
+    batch_size : number of mini-batch samples
+    verbose: print intermediate results
+    
+    returns metrics: list with train and test accuracy"""
+    
+    title = 'model ' + model_ref().name + ' - ' \
+            + str(len(dataset)) + ' fold cross-validation'
+    
+    print(title, '\n')
+    
+    dataset_records = list(dataset.keys())
+    
+    stats = []
+    
+    for idx_record, record in enumerate(dataset_records):
+      
+        records_train = [record]
+        records_test = [key for key in dataset_records 
+                            if key not in records_train]
+        
+        generator_train = PSGSequence(dataset, 
+                                      records=records_train, 
+                                      batch_size=batch_size,
+                                      tensor=tensor)
+
+        generator_test = PSGSequence(dataset, 
+                                     records=records_test,
+                                     batch_size=1024,
+                                     tensor=tensor)
+        
+        x_shape = generator_train.x_batch_shape[1:]
+        y_shape = generator_train.y_batch_shape[1:]
+
+        model = model_ref(x_shape, y_shape)
+        
+        stats_run_init_train = model.evaluate_generator(generator_train)
+        stats_run_init_test = model.evaluate_generator(generator_test)
+        
+        stats_run = {}
+        for idx_metric, metric in enumerate(model.metrics_names):
+            
+            stats_run[metric] = [stats_run_init_train[idx_metric]]
+            stats_run['val_'+metric] = [stats_run_init_test[idx_metric]]
+          
+        history = model.fit_generator(generator_train, 
+                                      epochs=epochs, 
+                                      validation_data=generator_test,
+                                      verbose=False)
+
+        for idx_metric, metric in enumerate(model.metrics_names):
+
+            stats_run[metric] += history.history[metric]
+            stats_run['val_'+metric] += history.history['val_'+metric]
+
+        stats += [stats_run]
+
+    if plot:
+      
+        plot_cross_validation(stats, model_ref().metrics_names, title=title)
+        
+    return stats
+  
+  
+def plot_cross_validation(stats, metrics_names,
+                          metrics_short=metrics_short_ref,
+                          title=None, ylim=None, 
+                          fig_w=4.5, fig_h=3.5):
+
+    fig, axs = plt.subplots(1, 2, figsize=(2*fig_w, fig_h))
+
+    if title is not None:
+        fig.suptitle(title, fontsize=14, y=0.99)
+
+    for idx_metric, metric in enumerate(metrics_names):
+      
+        if metric not in metrics_short:
+          
+            metrics_short[metric] = metric
+            
+        data = np.array([item[metric] for item in stats])
+        data_val = np.array([item['val_'+metric] for item in stats])
+
+        if idx_metric==0:
+            labels = ['train', 'val']
+        else:
+            labels = [None, None]
+
+
+
+        plt.sca(axs[idx_metric])
+        plt.plot(data[0], 'C0', alpha=0.1)
+        plt.plot(data[1:].T, 'C0', alpha=0.1)
+        plt.plot(np.mean(data, axis=0), 'C0', linewidth=2, label=labels[0])
+
+        plt.plot(data_val[0], 'C1', alpha=0.1)
+        plt.plot(data_val[1:].T, 'C1', alpha=0.1)
+        plt.plot(np.mean(data_val, axis=0), 'C1', linewidth=2, label=labels[1])
+
+        plt.title(metrics_short[metric])
+        plt.ylabel(metrics_short[metric])
+        plt.xlabel('epochs')
+
+        if idx_metric==0:
+            plt.legend()
+
+    plt.tight_layout()
+    fig.canvas.draw()
+    plt.show()
+    
+    return fig
+
+
+def plot_dataset_stats(dataset, exclude_record=None, title=None,
         x_min=-1, x_max=1, y_max=None, x_n=5e4):
     """Plot per channel histograms of PSG record in dataset.
     dataset: dataset dictionary with PSG channels with shape (batch, channel, data)
